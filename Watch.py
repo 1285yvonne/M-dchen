d227dc0f7073ac15817bd79e03de978333e08ef9
lilinhan <lilinhan1303@gmail.com>
2016-08-12 17:34:02 +0800

    test

--- Watch.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-
-def hello():
-    str = "stan is guasong";
-    print(str);
-
-if __name__ == "__main__":
-    hello()
---
+++ js/index.js
@@ -0,0 +1,6 @@
+function helloworld() {
+    alert("同学，你知道就好!");
+}
+function nope() {
+    alert("你不诚实奥!");
+}
